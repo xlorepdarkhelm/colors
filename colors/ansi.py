f5a8e82ae51681315e9dff328cebd70d58ee3237
Cliff Hill <github@darkhelm.org>
2017-08-24 10:52:30 -0400

    Adding documentation, filled out ANSI color group more.

--- colors/ansi.py
+++ colors/ansi.py
@@ -1,22 +1,156 @@
+"""Contains the implementation of the ANSI color group."""
+__all__ = (
+    'ANSI',
+)
+
 import enum
 
 from colors import base
 
-class ANSI(base.ColorGroup):
-    Normal_Black   = base.RGBColor(  0,   0,   0)
-    Normal_Red     = base.RGBColor(128,   0,   0)
-    Normal_Green   = base.RGBColor(  0, 128,   0)
-    Normal_Yellow  = base.RGBColor(128, 128,   0)
-    Normal_Blue    = base.RGBColor(  0,   0, 128)
-    Normal_Magenta = base.RGBColor(128,   0, 128)
-    Normal_Cyan    = base.RGBColor(  0, 128, 128)
-    Normal_White   = base.RGBColor(192, 192, 192)
 
-    Bright_Black   = base.RGBColor(128, 128, 128)
-    Bright_Red     = base.RGBColor(255,   0,   0)
-    Bright_Green   = base.RGBColor(  0, 255,   0)
-    Bright_Yellow  = base.RGBColor(255, 255,   0)
-    Bright_Blue    = base.RGBColor(  0,   0, 255)
-    Bright_Magenta = base.RGBColor(255,   0, 255)
-    Bright_Cyan    = base.RGBColor(  0, 255, 255)
-    Bright_White   = base.RGBColor(255, 255, 255)
+class ANSI(base.ColorGroup):
+    """
+    The color group for ANSI escape sequences.
+
+    The original specification for ANSI escape sequences had only 8 colors,
+    and gave them names. Several terminals implemented "bold" colors as
+    brighter colors rather than a different font, providing 8 additional
+    foreground colors, however you usually could not get these as background
+    colors (there was a trick that sometimes worked using inverse video
+    codes).
+
+    One of the fundamental problems with these colors is that different
+    terminal emulators implement these as different RGB values.  As a result
+    I have implemented this group to handle the values from multiple
+    different terminals.
+
+    See Also:
+        `Wikipedia <https://en.wikipedia.org/wiki/ANSI_escape_code#Colors>`
+
+    """
+
+    VGA_Black   = base.RGBColor(  0,   0,   0)
+    VGA_Red     = base.RGBColor(170,   0,   0)
+    VGA_Green   = base.RGBColor(  0, 170,   0)
+    VGA_Yellow  = base.RGBColor(170,  85,   0)
+    VGA_Blue    = base.RGBColor(  0,   0, 170)
+    VGA_Magenta = base.RGBColor(170,   0, 170)
+    VGA_Cyan    = base.RGBColor(  0, 170, 170)
+    VGA_White   = base.RGBColor(170, 170, 170)
+
+    VGA_BrightBlack   = base.RGBColor( 85,  85,  85)
+    VGA_BrightRed     = base.RGBColor(255,  85,  85)
+    VGA_BrightGreen   = base.RGBColor( 85, 255,  85)
+    VGA_BrightYellow  = base.RGBColor(255, 255,  85)
+    VGA_BrightBlue    = base.RGBColor( 85,  85, 255)
+    VGA_BrightMagenta = base.RGBColor(255,  85, 255)
+    VGA_BrightCyan    = base.RGBColor( 85, 255, 255)
+    VGA_BrightWhite   = base.RGBColor(255, 255, 255)
+
+    CMD_Black   = base.RGBColor(  0,   0,   0)
+    CMD_Red     = base.RGBColor(128,   0,   0)
+    CMD_Green   = base.RGBColor(  0, 128,   0)
+    CMD_Yellow  = base.RGBColor(128, 128,   0)
+    CMD_Blue    = base.RGBColor(  0,   0, 128)
+    CMD_Magenta = base.RGBColor(128,   0, 128)
+    CMD_Cyan    = base.RGBColor(  0, 128, 128)
+    CMD_White   = base.RGBColor(192, 192, 192)
+
+    CMD_BrightBlack   = base.RGBColor(128, 128, 128)
+    CMD_BrightRed     = base.RGBColor(255,   0,   0)
+    CMD_BrightGreen   = base.RGBColor(  0, 255,   0)
+    CMD_BrightYellow  = base.RGBColor(255, 255,   0)
+    CMD_BrightBlue    = base.RGBColor(  0,   0, 255)
+    CMD_BrightMagenta = base.RGBColor(255,   0, 255)
+    CMD_BrightCyan    = base.RGBColor(  0, 255, 255)
+    CMD_BrightWhite   = base.RGBColor(255, 255, 255)
+
+    Terminal_app_Black   = base.RGBColor(  0,   0,   0)
+    Terminal_app_Red     = base.RGBColor(194,  54,  33)
+    Terminal_app_Green   = base.RGBColor( 37, 188,  36)
+    Terminal_app_Yellow  = base.RGBColor(173, 173,  39)
+    Terminal_app_Blue    = base.RGBColor( 73,  46, 255)
+    Terminal_app_Magenta = base.RGBColor(211,  56, 211)
+    Terminal_app_Cyan    = base.RGBColor( 51, 187, 200)
+    Terminal_app_White   = base.RGBColor(203, 204, 205)
+
+    Terminal_app_BrightBlack   = base.RGBColor(129, 131, 131)
+    Terminal_app_BrightRed     = base.RGBColor(252,  57,  31)
+    Terminal_app_BrightGreen   = base.RGBColor( 49, 231,  34)
+    Terminal_app_BrightYellow  = base.RGBColor(234, 236,  35)
+    Terminal_app_BrightBlue    = base.RGBColor( 88,  51, 255)
+    Terminal_app_BrightMagenta = base.RGBColor(249,  53, 248)
+    Terminal_app_BrightCyan    = base.RGBColor( 20, 240, 240)
+    Terminal_app_BrightWhite   = base.RGBColor(233, 235, 235)
+
+    PuTTY_Black   = base.RGBColor(  0,   0,   0)
+    PuTTY_Red     = base.RGBColor(187,   0,   0)
+    PuTTY_Green   = base.RGBColor(  0, 187,   0)
+    PuTTY_Yellow  = base.RGBColor(187, 187,   0)
+    PuTTY_Blue    = base.RGBColor(  0,   0, 187)
+    PuTTY_Magenta = base.RGBColor(187,   0, 187)
+    PuTTY_Cyan    = base.RGBColor(  0, 187, 187)
+    PuTTY_White   = base.RGBColor(187, 187, 187)
+
+    PuTTY_BrightBlack   = base.RGBColor( 85,  85,  85)
+    PuTTY_BrightRed     = base.RGBColor(255,  85,  85)
+    PuTTY_BrightGreen   = base.RGBColor( 85, 255,  85)
+    PuTTY_BrightYellow  = base.RGBColor(255, 255,  85)
+    PuTTY_BrightBlue    = base.RGBColor( 85,  85, 255)
+    PuTTY_BrightMagenta = base.RGBColor(255,  85, 255)
+    PuTTY_BrightCyan    = base.RGBColor( 85, 255, 255)
+    PuTTY_BrightWhite   = base.RGBColor(255, 255, 255)
+
+    mIRC_Black   = base.RGBColor(  0,   0,   0)
+    mIRC_Red     = base.RGBColor(127,   0,   0)
+    mIRC_Green   = base.RGBColor(  0, 147,   0)
+    mIRC_Yellow  = base.RGBColor(252, 127,   0)
+    mIRC_Blue    = base.RGBColor(  0,   0, 127)
+    mIRC_Magenta = base.RGBColor(156,   0, 156)
+    mIRC_Cyan    = base.RGBColor(  0, 147, 147)
+    mIRC_White   = base.RGBColor(210, 210, 210)
+
+    mIRC_BrightBlack   = base.RGBColor(127, 127, 127)
+    mIRC_BrightRed     = base.RGBColor(255,   0,   0)
+    mIRC_BrightGreen   = base.RGBColor(  0, 255,   0)
+    mIRC_BrightYellow  = base.RGBColor(255, 255,   0)
+    mIRC_BrightBlue    = base.RGBColor(  0,   0, 255)
+    mIRC_BrightMagenta = base.RGBColor(255,   0, 255)
+    mIRC_BrightCyan    = base.RGBColor(  0, 255, 255)
+    mIRC_BrightWhite   = base.RGBColor(255, 255, 255)
+
+    xterm_Black   = base.RGBColor(  0,   0,   0)
+    xterm_Red     = base.RGBColor(205,   0,   0)
+    xterm_Green   = base.RGBColor(  0, 205,   0)
+    xterm_Yellow  = base.RGBColor(205, 205,   0)
+    xterm_Blue    = base.RGBColor(  0,   0, 238)
+    xterm_Magenta = base.RGBColor(205,   0, 205)
+    xterm_Cyan    = base.RGBColor(  0, 205, 205)
+    xterm_White   = base.RGBColor(229, 229, 229)
+
+    xterm_BrightBlack   = base.RGBColor(127, 127, 127)
+    xterm_BrightRed     = base.RGBColor(255,   0,   0)
+    xterm_BrightGreen   = base.RGBColor(  0, 255,   0)
+    xterm_BrightYellow  = base.RGBColor(255, 255,   0)
+    xterm_BrightBlue    = base.RGBColor( 92,  92, 255)
+    xterm_BrightMagenta = base.RGBColor(255,   0, 255)
+    xterm_BrightCyan    = base.RGBColor(  0, 255, 255)
+    xterm_BrightWhite   = base.RGBColor(255, 255, 255)
+
+    X_Black   = base.RGBColor(  0,   0,   0)
+    X_Red     = base.RGBColor(255,   0,   0)
+    X_Green   = base.RGBColor(  0, 255,   0)
+    X_Yellow  = base.RGBColor(255, 255,   0)
+    X_Blue    = base.RGBColor(  0,   0, 255)
+    X_Magenta = base.RGBColor(255,   0, 255)
+    X_Cyan    = base.RGBColor(  0, 255, 255)
+    X_White   = base.RGBColor(255, 255, 255)
+
+    X_BrightBlack   = None
+    X_BrightRed     = None
+    X_BrightGreen   = base.RGBColor(144, 238, 144)
+    X_BrightYellow  = base.RGBColor(255, 255, 224)
+    X_BrightBlue    = base.RGBColor(173, 216, 230)
+    X_BrightMagenta = None
+    X_BrightCyan    = base.RGBColor(224, 255, 255)
+    X_BrightWhite   = None
--- colors/base.py
+++ colors/base.py
@@ -2,6 +2,7 @@
 
 import collections.abc
 import colorsys
+import contextlib
 import enum
 import functools
 import typing
@@ -19,19 +20,39 @@ class RGBColor(
 ):
     """
     Class that defines a red/green/blue (RGB) color.
-    
-    This is a namedtuple that simply contains 3 8-bit (0-255) values that represent
-    red, green, and blue values for the color. It is possible to convert this to
-    :py:class:`HSVColor` or :py:class:`HSLColor` through conversion properties
-    (:py:meth:`hsv` & :py:meth:`hsl` respectively).
-    
+
+    This is a namedtuple that simply contains 3 8-bit (0-255) values that
+    represent red, green, and blue values for the color. It is possible to
+    convert this to :py:class:`HSVColor` or :py:class:`HSLColor` through
+    conversion properties (:py:meth:`hsv` & :py:meth:`hsl` respectively).
+
     Attributes:
-        red (int): An integer value from 0-255 representing the red level for the color.
-        green (int): An integer value from 0-255 representing the green level for the color.
-        blue (int): An integer value from 0-255 representing the blue level for the color.
+        red (int): An integer value from 0-255 representing the red level for
+            the color.
+        green (int): An integer value from 0-255 representing the green level
+            for the color.
+        blue (int): An integer value from 0-255 representing the blue level
+            for the color.
+
     """
+
     def __new__(cls, red: int, green: int, blue: int) -> 'RGBColor':
-        """Validate that the red, green, and blue colors are in the correct range."""
+        """
+        Validate red, green, and blue then construct the instance.
+
+        Parameters:
+            red (int): Integer value in the range 0-255.
+            green (int): Integer value in the range 0-255.
+            blue (int): Integer value in the range 0-255.
+
+        Returns:
+            RGBColor: The RGBColor instance.
+
+        Raises:
+            ValueError: If `red`, `green`, or `blue` are outside of the
+            required range.
+
+        """
         red, green, blue = int(red), int(green), int(blue)
         red_good = 0 <= red <= 255
         green_good = 0 <= green <= 255
@@ -45,7 +66,7 @@ class RGBColor(
 
     @property
     def hsv(self) -> 'HSVColor':
-        """The HSV translation of this color."""
+        """HSV translation of this color."""
         self.__hsv: HSVColor
         try:
             return self.__hsv
@@ -55,7 +76,7 @@ class RGBColor(
 
     @property
     def hsl(self) -> 'HSLColor':
-        """The HSL translation of this color.
+        """HSL translation of this color."""
         self.__hsl: HSLColor
         try:
             return self.__hsl
@@ -128,23 +149,53 @@ class HSVColor(
 ):
     """
     Class that defines a hue/saturation/value (HSV) color.
-    
-    HSV is a common cylendrical-coordinate representation of colors. This form for colors is an attempt
-    to make it easier to perceive the colors in a more three-dimensional model, often used by color pickers and
-    television broadcasts.
-    
+
+    HSV is a common cylendrical-coordinate representation of colors. This
+    form for colors is an attempt to make it easier to perceive the colors
+    in a more three-dimensional model, often used by color pickers and
+    television broadcasts. It is possible to convert this to
+    :py:class:`RGBColor` or :py:class:`HSLColor` through conversion
+    properties (:py:meth:`rgb` & :py:meth:`hsl` respectively).
+
     Attributes:
-        hue (int): An integer value from 0-359 representing an angle of rotation in the color cylender.
-            Each value represents a different color hue. If the colors go below 0, they are automatically wrapped
-            around (add 360 to the value) to know the true value. Likewise, if they exceed 359, they are wrapped
-            (subtract 360 from the value) to know the true value.
-        saturation (int): An integer value from 0-100 representing the color saturation for the given hue. A lower
-            value results in less color (more gray), while a higher value results in more color (less gray) being shown.
-        value (int): An integer value from 0-100 representing the brightness of the color. Lower values approach black,
-            while higher values approach the brightest color.
+        hue (int): An integer value from 0-359 representing an angle of
+            rotation in the color cylender. Each value represents a different
+            color hue. If the colors go below 0, they are automatically
+            wrapped around (add 360 to the value) to know the true value.
+            Likewise, if they exceed 359, they are wrapped (subtract 360 from
+            the value) to know the true value.
+        saturation (int): An integer value from 0-100 representing the color
+            saturation for the given hue. A lower value results in less color
+            (more gray), while a higher value results in more color (less
+            gray) being shown.
+        value (int): An integer value from 0-100 representing the brightness
+            of the color. Lower values approach black, while higher values
+            approach the brightest color.
+
     """
+
     def __new__(cls, hue: int, saturation: int, value: int) -> 'HSVColor':
-        """Validate that the hue, saturation, and value integers are in the correct range."""
+        """
+        Validate hue, saturation, and value then construct the instance.
+
+        Parameters:
+            hue (int): Integer value in the range 0-359.
+            saturation (int): Integer value in the range 0-100.
+            value (int): Integer value in the range 0-100.
+
+        Returns:
+            HSVColor: The HSVColor instance.
+
+        Raises:
+            ValueError: If `saturation` or `value` are outside of the
+            required range.
+
+        Notes:
+            If `hue` is outside the required range, the value either has 360
+            added or subtraced from it until the value is in the required
+            range.
+
+        """
         hue, saturation, value = int(hue), int(saturation), int(value)
         while hue >= 360:
             hue -= 360
@@ -161,7 +212,7 @@ class HSVColor(
 
     @property
     def rgb(self) -> 'RGBColor':
-        """The RGB translation of this color."""
+        """RGB translation of this color."""
         self.__rgb: RGBColor
         try:
             return self.__rgb
@@ -171,7 +222,7 @@ class HSVColor(
 
     @property
     def hsl(self) -> 'HSLColor':
-        """The HSL translation of this color."""
+        """HSL translation of this color."""
         self.__hsl: HSLColor
         try:
             return self.__hsl
@@ -203,23 +254,53 @@ class HSLColor(
 ):
     """
     Class that defines a hue/saturation/lightness (HSL) color.
-    
-    HSL is a common cylendrical-coordinate representation of colors. This form for colors is an attempt
-    to make it easier to perceive the colors in a more three-dimensional model, often used by color pickers and
-    television broadcasts.
-    
+
+    HSL is a common cylendrical-coordinate representation of colors. This
+    form for colors is an attempt to make it easier to perceive the colors
+    in a more three-dimensional model, often used by color pickers and
+    television broadcasts. It is possible to convert this to
+    :py:class:`RGBColor` or :py:class:`HSVColor` through conversion
+    properties (:py:meth:`rgb` & :py:meth:`hsv` respectively).
+
     Attributes:
-        hue (int): An integer value from 0-359 representing an angle of rotation in the color cylender.
-            Each value represents a different color hue. If the colors go below 0, they are automatically wrapped
-            around (add 360 to the value) to know the true value. Likewise, if they exceed 359, they are wrapped
-            (subtract 360 from the value) to know the true value.
-        saturation (int): An integer value from 0-100 representing the color saturation for the given hue. A lower
-            value results in less color (more gray), while a higher value results in more color (less gray) being shown.
-        lightness (int): An integer value from 0-100 representing the lightness of the color. Lower values approach black,
-            while higher values approach white.
+        hue (int): An integer value from 0-359 representing an angle of
+            rotation in the color cylender. Each value represents a different
+            color hue. If the colors go below 0, they are automatically
+            wrapped around (add 360 to the value) to know the true value.
+            Likewise, if they exceed 359, they are wrapped (subtract 360 from
+            the value) to know the true value.
+        saturation (int): An integer value from 0-100 representing the color
+            saturation for the given hue. A lower value results in less color
+            (more gray), while a higher value results in more color (less
+            gray) being shown.
+        lightness (int): An integer value from 0-100 representing the
+            lightness of the color. Lower values approach black, while higher
+            values approach white.
+
     """
+
     def __new__(cls, hue: int, saturation: int, lightness: int) -> 'HSLColor':
-        """Validate that the hue, saturation, and lightness integers are in the correct range."""
+        """
+        Validate hue, saturation, and lightness then construct the instance.
+
+        Parameters:
+            hue (int): Integer value in the range 0-359.
+            saturation (int): Integer value in the range 0-100.
+            lightness (int): Integer value in the range 0-100.
+
+        Returns:
+            HSLColor: The HSLColor instance.
+
+        Raises:
+            ValueError: If `saturation` or `lightness` are outside of the
+            required range.
+
+        Notes:
+            If `hue` is outside the required range, the value either has 360
+            added or subtraced from it until the value is in the required
+            range.
+
+        """
         hue, saturation, lightness = int(hue), int(saturation), int(lightness)
         while hue >= 360:
             hue -= 360
@@ -235,7 +316,7 @@ class HSLColor(
             )
     @property
     def rgb(self) -> 'RGBColor':
-        """The RGB translation of this color."""
+        """RGB translation of this color."""
         self.__rgb: RGBColor
         try:
             return self.__rgb
@@ -245,7 +326,7 @@ class HSLColor(
 
     @property
     def hsv(self) -> 'HSVColor':
-        """The HSV translation of this color."""
+        """HSV translation of this color."""
         self.__hsv: HSVColor
         try:
             return self.__hsv
@@ -257,7 +338,7 @@ class HSLColor(
     @functools.lru_cache(maxsize=128)
     def from_rgb(cls, rgb: 'RGBColor') -> 'HSLColor':
         """Convert the RGBColor to HSLColor."""
-         hue, lightness, saturation = colorsys.rgb_to_hls(
+        hue, lightness, saturation = colorsys.rgb_to_hls(
             rgb.red / 255,
             rgb.green / 255,
             rgb.blue / 255,
@@ -268,15 +349,21 @@ class HSLColor(
 class ColorGroup(enum.Enum):
     """
     Base class used to implement color groups.
-    
-    Color groups are defined as Enums of named :py:class:`RGBColor` values. The purpose
-    of these is to logically manage colors more easily, as well as provide a mechanism to
-    convert a color from one system to the closest color of another system in a simple way.
+
+    Color groups are defined as Enums of named :py:class:`RGBColor` values.
+    The purpose of these is to logically manage colors more easily, as well
+    as provide a mechanism to convert a color from one system to the closest
+    color of another system in a simple way.
     """
+
     @classmethod
     @functools.lru_cache(maxsize=128)
-    def closest(cls, rgb: RGBColor) -> ColorGroup:
-        """Find the color in this group that most closely matches the given :py:class:`RGBColor`."""
+    def closest(cls, color: typing.Union[ColorGroup, RGBColor]) -> ColorGroup:
+        """Find the closest match in this color group to the given color."""
+        try:
+            rgb = color.value  # type: ignore
+        except AttributeError:
+            rgb = color
         try:
             return cls(rgb)
         except ValueError:
@@ -292,7 +379,7 @@ class ColorGroup(enum.Enum):
 
     @property
     def index(self):
-        """Returns the numerical index value for color in this group."""
+        """Index value for color in this group."""
         try:
             return self.__index
         except AttributeError:
