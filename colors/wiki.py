fbe01936b4b5465399b9fd711f304229954f0057
Cliff Hill <xlorepdarkhelm@users.noreply.github.com>
2017-09-08 14:45:51 -0400

    Update wiki.py

--- colors/wiki.py
+++ colors/wiki.py
@@ -13,12 +13,12 @@ class Wiki(base.ColorGroup):
     The Wiki color group.
 
     The following is a list of colors. A number of the color swatches below are
-taken from domain-specific naming schemes such as X11 or HTML4. RGB values are
-given for each swatch because such standards are defined in terms of the sRGB
-color space. It is not possible to accurately convert many of these swatches to
-CMYK values because of the differing gamuts of the two spaces, but the color
-management systems built into operating systems and image editing software
-attempt such conversions as accurately as possible.
+    taken from domain-specific naming schemes such as X11 or HTML4. RGB values
+    are given for each swatch because such standards are defined in terms of
+    the sRGB color space. It is not possible to accurately convert many of
+    these swatches to CMYK values because of the differing gamuts of the two
+    spaces, but the color management systems built into operating systems and
+    image editing software attempt such conversions as accurately as possible.
 
     See Also:
         Wikipedia List of Colors
