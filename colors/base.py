23c7ccb2af57b9134f9aa6c58edfb2d2ce9cdab5
Cliff Hill <xlorepdarkhelm@users.noreply.github.com>
2017-09-14 14:47:21 -0400

    Update base.py

--- colors/base.py
+++ colors/base.py
@@ -1,23 +1,13 @@
 """Base module containing the core components for the colors system."""
 
-import collections.abc
-import colorsys
-import contextlib
 import enum
 import functools
 import typing
 
+import colormath
 
-class RGBColor(
-    typing.NamedTuple(
-        'RGBColor',
-        (
-            ('red', int),
-            ('green', int),
-            ('blue', int),
-        )
-    )
-):
+
+class sRGBColor(colormath.color_objects.sRGBColor):
     """
     Class that defines a red/green/blue (RGB) color.
 
@@ -35,35 +25,6 @@ class RGBColor(
             for the color.
 
     """
-
-    def __new__(cls, red: int, green: int, blue: int) -> 'RGBColor':
-        """
-        Validate red, green, and blue then construct the instance.
-
-        Parameters:
-            red (int): Integer value in the range 0-255.
-            green (int): Integer value in the range 0-255.
-            blue (int): Integer value in the range 0-255.
-
-        Returns:
-            RGBColor: The RGBColor instance.
-
-        Raises:
-            ValueError: If `red`, `green`, or `blue` are outside of the
-            required range.
-
-        """
-        red, green, blue = int(red), int(green), int(blue)
-        red_good = 0 <= red <= 255
-        green_good = 0 <= green <= 255
-        blue_good = 0 <= blue <= 255
-        if all((red_good, green_good, blue_good)):
-            return super().__new__(cls, red, green, blue)
-        else:
-            raise ValueError(
-                'Red, green, and blue must be in the range 0 -> 255.'
-            )
-
     @property
     def hsv(self) -> 'HSVColor':
         """HSV translation of this color."""
@@ -71,7 +32,7 @@ class RGBColor(
         try:
             return self.__hsv
         except AttributeError:
-            self.__hsv = HSVColor.from_rgb(self)
+            self.__hsv = colormath.color_conversions.convert_color(self, HSVColor)
             return self.__hsv
 
     @property
@@ -81,72 +42,20 @@ class RGBColor(
         try:
             return self.__hsl
         except AttributeError:
-            self.__hsl = HSLColor.from_rgb(self)
+            self.__hsl = colormath.color_conversions.convert_color(self, HSLColor)
             return self.__hsl
 
     @classmethod
     @functools.lru_cache(maxsize=128)
-    def from_hsv(cls, hsv: 'HSVColor') -> 'RGBColor':
-        """Convert the HSVColor to RGBColor."""
-        red, green, blue = colorsys.hsv_to_rgb(
-            hsv.hue / 360,
-            hsv.saturation / 100,
-            hsv.value / 100,
-        )
-        return cls(int(red * 255), int(green * 255), int(blue * 255))
-
-    @classmethod
-    @functools.lru_cache(maxsize=128)
-    def from_hsl(cls, hsl: 'HSLColor') -> 'RGBColor':
-        """Convert the HSLColor to RGBColor."""
-        red, green, blue = colorsys.hls_to_rgb(
-            hsl.hue / 360,
-            hsl.lightness / 100,
-            hsl.saturation / 100,
-        )
-        return cls(int(red * 255), int(green * 255), int(blue * 255))
-
-    def __str__(self) -> str:
-        """Convert this RGBColor into web/hex notation string format."""
-        self.__str: str
-        try:
-            return self.__str
-        except AttributeError:
-            self.__str = f'#{self.red:02X}{self.green:02X}{self.blue:02X}'
-            return self.__str
-
-    @classmethod
-    @functools.lru_cache(maxsize=128)
-    def from_str(cls, web_str: str) -> 'RGBColor':
-        """Convert the web/hex notation string into RGBColor."""
-        try:
-            if web_str[0] == '#':
-                web_str = web_str[1:]
-        except IndexError:
-            raise ValueError('Cannot process empty strings.')
-        if len(web_str) not in {3, 6}:
-            raise ValueError('String must either be 3 or 6 characters long.')
-        elif len(web_str) == 6:
-            red, green, blue = tuple(
-                int(web_str[ndx:ndx+2], 16)
-                for ndx in range(0, 5, 2)
-            )
-            return cls(red, green, blue)
-        else:
-            red, green, blue = tuple(int(value, 16) << 4 for value in web_str)
-            return cls(red, green, blue)
+    def from_str(cls, hex_str: str) -> 'sRGBColor':
+        """Convert the web/hex notation string into sRGBColor."""
+        return cls.new_from_rgb_hex(hex_str)
+    
+    def __str__(self):
+        return self.get_rgb_hex()
 
 
-class HSVColor(
-    typing.NamedTuple(
-        'HSVColor',
-        (
-            ('hue', int),
-            ('saturation', int),
-            ('value', int),
-        )
-    )
-):
+class HSVColor(colormath.color_objects.HSVColor):
     """
     Class that defines a hue/saturation/value (HSV) color.
 
@@ -154,8 +63,8 @@ class HSVColor(
     form for colors is an attempt to make it easier to perceive the colors
     in a more three-dimensional model, often used by color pickers and
     television broadcasts. It is possible to convert this to
-    :py:class:`RGBColor` or :py:class:`HSLColor` through conversion
-    properties (:py:meth:`rgb` & :py:meth:`hsl` respectively).
+    :py:class:`sRGBColor` or :py:class:`HSLColor` through conversion
+    properties (:py:meth:`srgb` & :py:meth:`hsl` respectively).
 
     Attributes:
         hue (int): An integer value from 0-359 representing an angle of
@@ -174,51 +83,15 @@ class HSVColor(
 
     """
 
-    def __new__(cls, hue: int, saturation: int, value: int) -> 'HSVColor':
-        """
-        Validate hue, saturation, and value then construct the instance.
-
-        Parameters:
-            hue (int): Integer value in the range 0-359.
-            saturation (int): Integer value in the range 0-100.
-            value (int): Integer value in the range 0-100.
-
-        Returns:
-            HSVColor: The HSVColor instance.
-
-        Raises:
-            ValueError: If `saturation` or `value` are outside of the
-            required range.
-
-        Notes:
-            If `hue` is outside the required range, the value either has 360
-            added or subtraced from it until the value is in the required
-            range.
-
-        """
-        hue, saturation, value = int(hue), int(saturation), int(value)
-        while hue >= 360:
-            hue -= 360
-        while hue < 0:
-            hue += 360
-        saturation_good = 0 <= saturation <= 100
-        value_good = 0 <= value <= 100
-        if all((saturation_good, value_good)):
-            return super().__new__(cls, hue, saturation, value)
-        else:
-            raise ValueError(
-                'Saturation and value must be in the range 0 -> 100.'
-            )
-
     @property
-    def rgb(self) -> 'RGBColor':
+    def srgb(self) -> 'sRGBColor':
         """RGB translation of this color."""
-        self.__rgb: RGBColor
+        self.__srgb: sRGBColor
         try:
-            return self.__rgb
+            return self.__srgb
         except AttributeError:
-            self.__rgb = RGBColor.from_hsv(self)
-            return self.__rgb
+            self.__srgb = colormath.color_conversions.convert_color(self, sRGBColor)
+            return self.__srgb
 
     @property
     def hsl(self) -> 'HSLColor':
@@ -227,31 +100,10 @@ class HSVColor(
         try:
             return self.__hsl
         except AttributeError:
-            self.__hsl = self.rgb.hsl
+            self.__hsl = colormath.color_conversions.convert_color(self, HSLColor)
             return self.__hsl
 
-    @classmethod
-    @functools.lru_cache(maxsize=128)
-    def from_rgb(cls, rgb: 'RGBColor') -> 'HSVColor':
-        """Convert the RGBColor to HSVColor."""
-        hue, saturation, value = colorsys.rgb_to_hsv(
-            rgb.red / 255,
-            rgb.green / 255,
-            rgb.blue / 255,
-        )
-        return cls(int(hue * 360), int(saturation * 100), int(value * 100))
-
-
-class HSLColor(
-    typing.NamedTuple(
-        'HSLColor',
-        (
-            ('hue', int),
-            ('saturation', int),
-            ('lightness', int),
-        )
-    )
-):
+class HSLColor(colormath.color_objects.HSLColor):
     """
     Class that defines a hue/saturation/lightness (HSL) color.
 
@@ -259,7 +111,7 @@ class HSLColor(
     form for colors is an attempt to make it easier to perceive the colors
     in a more three-dimensional model, often used by color pickers and
     television broadcasts. It is possible to convert this to
-    :py:class:`RGBColor` or :py:class:`HSVColor` through conversion
+    :py:class:`sRGBColor` or :py:class:`HSVColor` through conversion
     properties (:py:meth:`rgb` & :py:meth:`hsv` respectively).
 
     Attributes:
@@ -278,51 +130,15 @@ class HSLColor(
             values approach white.
 
     """
-
-    def __new__(cls, hue: int, saturation: int, lightness: int) -> 'HSLColor':
-        """
-        Validate hue, saturation, and lightness then construct the instance.
-
-        Parameters:
-            hue (int): Integer value in the range 0-359.
-            saturation (int): Integer value in the range 0-100.
-            lightness (int): Integer value in the range 0-100.
-
-        Returns:
-            HSLColor: The HSLColor instance.
-
-        Raises:
-            ValueError: If `saturation` or `lightness` are outside of the
-            required range.
-
-        Notes:
-            If `hue` is outside the required range, the value either has 360
-            added or subtraced from it until the value is in the required
-            range.
-
-        """
-        hue, saturation, lightness = int(hue), int(saturation), int(lightness)
-        while hue >= 360:
-            hue -= 360
-        while hue < 0:
-            hue += 360
-        saturation_good = 0 <= saturation <= 100
-        lightness_good = 0 <= lightness <= 100
-        if all((saturation_good, lightness_good)):
-            return super().__new__(cls, hue, saturation, lightness)
-        else:
-            raise ValueError(
-                'Saturation and lightness must be in the range 0 -> 100.'
-            )
     @property
-    def rgb(self) -> 'RGBColor':
-        """RGB translation of this color."""
-        self.__rgb: RGBColor
+    def srgb(self) -> 'sRGBColor':
+        """sRGB translation of this color."""
+        self.__srgb: sRGBColor
         try:
-            return self.__rgb
+            return self.__srgb
         except AttributeError:
-            self.__rgb = RGBColor.from_hsl(self)
-            return self.__rgb
+            self.__srgb = colormath.color_conversions.convert_color(self, sRGBColor)
+            return self.__srgb
 
     @property
     def hsv(self) -> 'HSVColor':
@@ -331,26 +147,15 @@ class HSLColor(
         try:
             return self.__hsv
         except AttributeError:
-            self.__hsv = self.rgb.hsv  # type: ignore
+            self.__hsv = colormath.color_conversions.convert_color(self, HSVColor)
             return self.__hsv
-
-    @classmethod
-    @functools.lru_cache(maxsize=128)
-    def from_rgb(cls, rgb: 'RGBColor') -> 'HSLColor':
-        """Convert the RGBColor to HSLColor."""
-        hue, lightness, saturation = colorsys.rgb_to_hls(
-            rgb.red / 255,
-            rgb.green / 255,
-            rgb.blue / 255,
-        )
-        return cls(int(hue * 360), int(saturation * 100), int(lightness * 100))
 
 
 class ColorGroup(enum.Enum):
     """
     Base class used to implement color groups.
 
-    Color groups are defined as Enums of named :py:class:`RGBColor` values.
+    Color groups are defined as Enums of named :py:class:`sRGBColor` values.
     The purpose of these is to logically manage colors more easily, as well
     as provide a mechanism to convert a color from one system to the closest
     color of another system in a simple way.
@@ -360,7 +165,7 @@ class ColorGroup(enum.Enum):
     @functools.lru_cache(maxsize=128)
     def closest(
         cls,
-        color: typing.Union['ColorGroup', RGBColor]
+        color: typing.Union['ColorGroup', sRGBColor]
     ) -> 'ColorGroup':
         """Find the closest match in this color group to the given color."""
         try:
